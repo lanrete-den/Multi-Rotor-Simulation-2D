3f7f198a8565995bf1ddfc5e32f7d1794866a85c
JustAToaster <danipcmedia@gmail.com>
2021-07-15 15:36:01 +0200

    Add utilities for loading graph from file

--- src/utilities.py
+++ src/utilities.py
@@ -3,5 +3,14 @@
 def readNodesCoordsAndEdges(fileName):
     f= open("nodes.txt","r")
     
+    nodes = dict()
+    edges = []
+
     fl =f.readlines()
-    for x in fl:
+    for line in fl:
+        elems = line.split(';')
+        nodes[elems[0]] = (int(elems[1].split(',')[0]), int(elems[1].split(',')[1]))
+        adj_list = elems[2].split(',')
+        for n in adj_list:
+            edges.append((elems[0], n))
+    return nodes, edges
