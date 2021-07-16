c9da2069dbe54938c5bd32d4feb0c3097db501c9
josura <giorgiolocicero@hotmail.com>
2021-07-16 10:56:19 +0200

    fixed some issues with utilities functions

--- src/utilities.py
+++ src/utilities.py
@@ -1,7 +1,7 @@
 import math
 
 def distanceCouple(tuple1,tuple2):
-    return math.sqrt((tuple1[0]-tuple2[0])^2 + (tuple1[1]-tuple2[1])^2)
+    return math.sqrt((tuple1[0]-tuple2[0])**2 + (tuple1[1]-tuple2[1])**2)
 
 def readNodesCoordsAndEdges(fileName):
     f= open(fileName,"r")
@@ -12,14 +12,16 @@ def readNodesCoordsAndEdges(fileName):
 
     fl =f.readlines()
     for line in fl:
-        elems = line.split(';')
-        nodes[elems[0]] = (int(elems[1].split(',')[0]), int(elems[1].split(',')[1]))
+        elems = line.replace("\n","").replace(" ","").split(';')
+        coords = elems[1].split(',')
+        nodes[elems[0]] = (int(coords[0]), int(coords[1]))
         adj_list = elems[2].split(',')
         for n in adj_list:
-            edgesTmp.append((n, elems[0]))
-            edgesTmp.append((elems[0], n))
+            if(elems[0] and n):
+                edgesTmp.append((n, elems[0]))
+                edgesTmp.append((elems[0], n))
     for edge in edgesTmp:
         distance = distanceCouple(nodes[edge[0]],nodes[edge[1]])   
-        edges.append((edge[0],edge[1],distance))
+        edges.append((edge[0],edge[1],int(math.ceil(distance))))
 
     return nodes, edges
